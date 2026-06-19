bab9b06198910a4246563aeb0e362acec3d07399
Hive Mind <hive-mind@tonbankcard.com>
2026-06-19 22:49:58 +0500

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+"""Setup script for backward compatibility.""
+from setuptools import setup, find_packages
+
+setup(
+    name="hive-mind",
+    version="0.1.0",
+    packages=find_packages(),
+    python_requires=">=3.10",
